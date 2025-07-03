73e290e1f63d0d73c8d99c005e979962f6230c99
Parvesh Shanmugam <parvesh.sm2023@vitstudent.ac.in>
2025-07-03 18:34:39 +0530

    Initial commit

---
+++ src/VRScene.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+const VRScene = () => (
+  <Canvas>
+    <OrbitControls />
+    <ambientLight intensity={0.5} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+    <mesh position={[0, 0, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="crimson" />
+    </mesh>
+  </Canvas>
+);
+
+export default VRScene;
